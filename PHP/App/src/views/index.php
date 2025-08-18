1d37f74a8050afe24de32d70d86c4bd9358a9703
John Patrick Ombao <www.ombaojohnpatrick@gmail.com>
2025-08-18 21:43:12 +0800

    php exercises

--- PHP/App/src/views/index.php
+++
@@ -1,4 +0,0 @@
-<form action="/upload" method="post" enctype="multipart/form-data">
-    <input type="file" name="receipt"/>
-    <button type="submit">Upload</button>
-</form>
--- PHP/App/src/views/invoices/create.php
+++
@@ -1,4 +0,0 @@
-<form method="post" action="/invoices/create">
-    <label>Amount</label>
-    <input type="text" name="amount">
-</form>
